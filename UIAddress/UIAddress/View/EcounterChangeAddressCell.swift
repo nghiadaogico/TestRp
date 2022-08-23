96dc85302f9106aec6d6a0c593dd9655d9b55af9
nghiadaogico <69239201+nghiadaogico@users.noreply.github.com>
2022-08-23 19:58:04 +0700

    test1

--- UIAddress/UIAddress/View/EcounterChangeAddressCell.swift
+++ UIAddress/UIAddress/View/EcounterChangeAddressCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class EcounterChangeAddressCell:UITableViewCell{
     
 
-    @IBOutlet weak var lbCellTitle: UILabel!
+    @IBOutlet weak var lbCellTitle: UILabel! 13123555
     @IBOutlet weak var lbHouseType: UILabel!
     
     @IBOutlet weak var vNormalHouseOption: EcounterHouseOptionSubView!
